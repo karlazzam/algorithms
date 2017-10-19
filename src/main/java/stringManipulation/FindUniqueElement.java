0952d154f5132a2deabf40dc45328eb3eacb4adf
Karl Azzam <karlazzam@yahoo.com>
2017-10-18 23:32:36 -0400

    find unique element method #3 can find unique element where there can be 3 or more duplicates of same element

--- src/main/java/stringManipulation/FindUniqueElement.java
+++ src/main/java/stringManipulation/FindUniqueElement.java
@@ -4,12 +4,12 @@ import java.util.*;
 
 public class FindUniqueElement {
 	
+	
 	/**
 	 * Find unique element in array where every element is duplicated except 1
 	 */
 
 	public int uniqueElementSolutionOne(int[] arr) {
-		
 		
 		if(arr.length == 1) {
 			return arr[0];
@@ -62,10 +62,59 @@ public class FindUniqueElement {
 		for(Integer element : arr) {
 			
 			result ^= element;
-			
 		}
 	
 		return result; 
 	}
+	
+	
+	/**
+	 * Find unique element in array where there is no unbounded number of duplicates for a given element
+	 * 
+	 * To do in O(n), need to use two hash sets:
+	 * one that adds all the keys to the set (key set)
+	 * one that will end up having the unique key (result set)
+	 * 
+	 * Then you check if the key set contains the key: O(1) operation
+	 * if so you remove it from result set
+	 * else you add it to both sets
+	 * 
+	 
+	 */
+	
+	public int uniqueElementSolutionThree(int[] arr) {
+		
+		if(arr.length == 1) {
+			return arr[0];
+		}
+		else if(arr.length == 0) {
+			return 0;
+		}
+		
+		Set<Integer> keySet = new HashSet<Integer>();
+		Set<Integer> resultSet = new HashSet<Integer>();
+		
+		for(Integer key : arr) {
+			
+			if(keySet.contains(key)) {
+				resultSet.remove(key); //remove key from result set if duplicate
+			}
+			else {
+				//add key to both sets if first time encountering this key
+				keySet.add(key); 
+				resultSet.add(key);
+			}
+		}
+
+		//result set will only have the unique key left, so return the unique key
+		if(resultSet.size() == 1) {
+			return resultSet.iterator().next(); 
+		}
+		else {
+			return 0; //no unique element found
+		}
+		
+	}
+	
 
 }
--- src/test/java/stringManipulationTests/FindUniqueElementTest.java
+++ src/test/java/stringManipulationTests/FindUniqueElementTest.java
@@ -11,20 +11,34 @@ public class FindUniqueElementTest {
 				
 		FindUniqueElement findUnique = new FindUniqueElement();
 		
-		int[] oneElement = {1};
+		int[] oneElement = {1}; //unique = 1
 		int[] unique4 = {1, 2, 3, 3, 1, 4, 5, 2, 5}; //unique = 4
 		int[] unique8 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 9, 10}; //unique = 8
 		int[] uniqueNeg8 = { -1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -1, -2, -3, -4, -5, -6, -7, -9, -10, 
-				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //unique = 8
-
+				1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //unique = -8
+		int[] unique1 = {2, 2, 2, 2, 2, 2, 2, 1, 3, 3, 3, 4, 4, 4, 5, 5}; //unique = 1
+		
 		assertEquals(1, findUnique.uniqueElementSolutionOne(oneElement));
 		assertEquals(1, findUnique.uniqueElementSolutionTwo(oneElement));
+		assertEquals(1, findUnique.uniqueElementSolutionThree(oneElement));
+		assertEquals(1, findUnique.uniqueElementSolutionThree(unique1));
+
+
 		assertEquals(4, findUnique.uniqueElementSolutionOne(unique4));
 		assertEquals(4, findUnique.uniqueElementSolutionTwo(unique4));
+		assertEquals(4, findUnique.uniqueElementSolutionThree(unique4));
+
+		
 		assertEquals(8, findUnique.uniqueElementSolutionOne(unique8));
 		assertEquals(8, findUnique.uniqueElementSolutionTwo(unique8));
+		assertEquals(8, findUnique.uniqueElementSolutionThree(unique8));
+
+		
 		assertEquals(-8, findUnique.uniqueElementSolutionOne(uniqueNeg8));
 		assertEquals(-8, findUnique.uniqueElementSolutionTwo(uniqueNeg8));
+		assertEquals(-8, findUnique.uniqueElementSolutionThree(uniqueNeg8));
+		
+
 
 		
 		
